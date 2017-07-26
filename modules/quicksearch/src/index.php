94e8125edad33e647f1f506eee75e88a36b1fee8
Simeon Taskaris <simos.taskaris@gmail.com>
2017-07-26 16:21:14 +0300

    Bug quicksearch module

--- modules/quicksearch/src/index.php
+++ modules/quicksearch/src/index.php
@@ -11,7 +11,7 @@ class quicksearch
 	var $searchStr="";
 	
 	var $_search = array(
-		
+
 		array(
 			"_serviceUrl"=>"",
 			"_layers"=>array(
@@ -22,52 +22,52 @@ class quicksearch
 			)
 		)
 	);
-	
+
 	function quicksearch($data)
 	{
 		$this->searchStr=$data["query"];
-	
+
 		$this->output=json_encode($this->quickSearchCreateResultsXML());
 	}
-	
+
 	function quickSearchCreateResultsXML()
 	{
 		libxml_use_internal_errors(true);
-		
+
 		require("../../../src/proxy/config.php");
-	
+
 		$f_response = array();
-	
+
 		foreach($this->_search as $key=>$value)
 		{
 			$_serviceUrl = $value["_serviceUrl"];
-			
+
 			$_layerName = implode(",",$value["_layers"]["_layerName"]);
-			
+
 			$_searchAtt = array();
-			
+
 			foreach($value["_layers"]["_searchAtt"] as $_k=>$_v)
 			{
 				$_searchAtt[] = $_v." LIKE '%".$this->searchStr."%'";
 			}
-			
+
 			$_cqlFilter = implode(" OR ", $_searchAtt);
-			
+
 			$p = new PROXY();
-			
+
 			$p->_url =  $this->quickSearchUrl($_layerName,$_serviceUrl,$_cqlFilter);
-			
+
 			$p->get();
-			
+
 			if ($p->o_responseCode == 200)
 			{
-			
+
 				$xml=new DOMDocument();
-				
+
 				$xml->loadXML($p->o_response);
-				
+
 				$xpath = new DOMXPath($xml);
-			
+
 				foreach($_config["NAMESPACES"] as $kn=>$vn)
 				{
 					$xpath->registerNamespace($kn,$vn);
@@ -84,60 +84,66 @@ class quicksearch
 					$nodes = $xpath->query(str_replace("[]","[".$i."]",$section["_featureMember"]));
 
 					$v = array();
-					
-					foreach ($nodes as $node) 
+
+					$_displayField = "";
+
+					foreach ($nodes as $node)
 					{
 						$_rslts = array();
-					
+
 						foreach($node->firstChild->childNodes as $n)
 						{
-							
+
 							if ($n->firstChild->namespaceURI!= $_config["NAMESPACES"]["gml"])
 							{
 								if (in_array($n->localName,$value["_layers"]["_resultAtt"]))
 								{
 									$_rslts[$n->localName]=$n->nodeValue;
 								}
-								
+								if($n->localName == $value["_layers"]["_displayField"])
+								{
+									$_displayField = $n->nodeValue;
+								}
+
 							}
 							else
 							{
 								$feature_srs = $n->firstChild->getAttribute("srsName");
 							}
 						}
-						
+
 						$_layerId = $node->firstChild->tagName;
-						
-						$_layerId = md5($_layerName);
-						
+
+						$_layerId = md5($_layerId);
+
 						$_serviceId = md5($_serviceUrl);
-						
+
 						$v["_layerId"] = $_serviceId.$_layerId;
-						
+
 						$v["_label"] = implode("<br>",$_rslts);
-					
+
 						$featureId = $node->firstChild->getAttribute("fid");
 					}
-					
+
 					$v["_featureId"]=$featureId;
 
 					$v["_srsName"]=$feature_srs;
-					
+
 					$v["_featureUrl"]=$this->createGetSearchUrl($_serviceUrl, $_layerName, $featureId);
-					
+
 					$v["_featureGeomFormat"]="gml";
 
 					$f_response[]=$v;
-					
+
 				}
-		
+
 			}
-			
+
 		}
-		
+
 		return $f_response;
 	}
-	
+
 	function createGetSearchUrl($_serviceUrl, $_layerName, $featureId)
     {
 		$_query["REQUEST"]="GETFEATURE";
@@ -145,17 +151,17 @@ class quicksearch
 		$_query["OUTPUTFORMAT"]="GML2";
 		$_query["TYPENAME"]=$_layerName;
 		$_query["FEATUREID"]=$featureId;
-		
+
         return Proxy::createUriQuery($_serviceUrl, $_query);
     }
-	
-	
+
+
 	function quickSearchUrl($_layerName,$_serviceUrl,$_cqlFilter)
 	{
 		$_query["REQUEST"]="GETFEATURE";
         $_query["SERVICE"]="WFS";
         $_query["OUTPUTFORMAT"]="GML2";
-		$_query["VERSION"]="1.0.0";
+		$_query["VERSION"]="2.0.0";
         $_query["TYPENAME"]=$_layerName;
         $_query["CQL_FILTER"]=str_replace("%","%25",$_cqlFilter);
        
